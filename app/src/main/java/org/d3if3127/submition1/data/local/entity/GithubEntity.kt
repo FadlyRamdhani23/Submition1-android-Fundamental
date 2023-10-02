c61db659933d0b4bfbf0cd35d445838aaac98212
FadlyRamdhani4503 <91235752+FadlyRamdhani4503@users.noreply.github.com>
2023-10-02 16:27:44 +0700

    button gambar

---
+++ app/src/main/java/org/d3if3127/submition1/data/local/entity/GithubEntity.kt
@@ -0,0 +1,12 @@
+package org.d3if3127.submition1.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "github")
+data class GithubEntity(
+    val login : String,
+    @PrimaryKey
+    val id : Int,
+): Serializable
